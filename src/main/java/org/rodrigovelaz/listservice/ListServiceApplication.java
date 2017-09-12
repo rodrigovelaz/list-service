8345bd56f3d6ce0a2560693c11ecd2d6b78684c0
rodrigovelaz <rodrigo.velaz@gmail.com>
2017-09-12 07:08:33 +0000

    Add @EnableDiscoveryClient

--- src/main/java/org/rodrigovelaz/listservice/ListServiceApplication.java
+++ src/main/java/org/rodrigovelaz/listservice/ListServiceApplication.java
@@ -3,6 +3,7 @@ package org.rodrigovelaz.listservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,9 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@ComponentScan
+@EnableDiscoveryClient
 @EnableJpaRepositories
 @EnableSwagger2
-@ComponentScan
 @EntityScan(basePackageClasses = {Jsr310JpaConverters.class}, basePackages = {"org.rodrigovelaz" })
 public class ListServiceApplication {
 
